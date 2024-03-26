bed558cf4265fdcb6279f5daec7d30bdb0b7beed
Gayle McClure <gaylekdennison@gmail.com>
2024-03-26 16:28:21 +1100

    added secret key

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -29,22 +29,9 @@ const getWeather = async () => {
 
 getWeather();
 
-// Write a function that takes in a string and returns true if it's a pangram or false otherwise.
-// Pangram: a sentence that contains every letter in the alphabet.
 
-// Ex:
-// Input: "Watch Jeopardy, Alex Trebek's fun TV quiz game"
-// Output: true
-
-// Input: "Five hexing wizard bots jump quickly"
-// Output: true
-
-// Input: "JavaScript is the best"
-// Output: false
-
-
-// fetch().then(function (response) {
-//   return response.json();
-// }) .then(function(data) {
-//   console.log(data)
+// const params = new URLSearchParams({
+//   something: "data",
+//   state: "something"
 // })
+//params.toString()
