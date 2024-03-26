33f0d78a8680c1b7baa8a438bebad30c655e8fa2
Gayle McClure <gaylekdennison@gmail.com>
2024-03-26 15:30:45 +1100

    updated fetch

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,10 +6,12 @@ const API_KEY = '57693c20cc9de93006be32fd645ff9bb';
 const getCoordinates = async () => {
   let city = "London";
   const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`
-  const res = await fetch(geoUrl)
-  const cordinates = await res.json();
-  console.log(cordinates)
-}
+ fetch(geoUrl).then(function(response) {
+  return response.json();
+ }).then(function(data) {
+  console.log(data)
+ })
+}; 
 
 getCoordinates();
 
@@ -17,11 +19,32 @@ const getWeather = async () => {
   let lat = '44.34';
   let lon = '10.99';
   const weatherUrl = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`
-  const res = await fetch(weatherUrl)
-  const weather = res.json();
-  console.log(weather)
-
+  fetch(weatherUrl).then(function(res) {
+    return res.json()
+  }).then(function(data) {
+    console.log(data)
+  })
 
 }
 
 getWeather();
+
+// Write a function that takes in a string and returns true if it's a pangram or false otherwise.
+// Pangram: a sentence that contains every letter in the alphabet.
+
+// Ex:
+// Input: "Watch Jeopardy, Alex Trebek's fun TV quiz game"
+// Output: true
+
+// Input: "Five hexing wizard bots jump quickly"
+// Output: true
+
+// Input: "JavaScript is the best"
+// Output: false
+
+
+// fetch().then(function (response) {
+//   return response.json();
+// }) .then(function(data) {
+//   console.log(data)
+// })
